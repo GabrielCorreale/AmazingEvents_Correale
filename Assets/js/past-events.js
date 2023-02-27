7e8adf6adf356dd488b423e33afe4b3c1646b857
GabrielCorreale <gabriel.correale@gmail.com>
2023-02-27 03:00:50 -0300

    Estilos agregados, menu hamburguesa terminado, mejora del responsive, display data agregado, resta details

---
+++ Assets/js/past-events.js
@@ -0,0 +1,30 @@
+
+let currentDateString = data.currentDate;
+let currentDate = new Date(currentDateString);
+
+let cards = document.getElementById('cards');
+let fragment = document.createDocumentFragment();
+
+for (let events of data.events) {
+
+    let eventeDateString = events.date;
+    let eventDate = new Date(eventeDateString);
+
+    if (eventDate < currentDate) {
+        let card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+        <img src="${events.image}" class="card-img" alt="...">
+        <div>
+            <h3 class="card-h3">${events.name}</h5>
+            <p class="card-p">${events.description}</p>
+        </div>
+        <div class="d-flex justify-content-evenly">
+            <p class="d-inline-block card-p">Price: $${events.price}</p>
+            <a href="./details.html" class="btn btn-dark color-text card-link">Details</a>
+        </div>`;
+        fragment.appendChild(card);
+    }
+}
+
+cards.appendChild(fragment);
